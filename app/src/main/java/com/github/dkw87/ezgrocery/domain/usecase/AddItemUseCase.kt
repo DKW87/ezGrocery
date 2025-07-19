3e3b6a30c50f2bf5cbfa3fc104cecfe166035729
DKW87 <dannykwant@gmail.com>
2025-07-19 21:29:09 +0200

    feat(AddItemUseCase): adds init

---
+++ app/src/main/java/com/github/dkw87/ezgrocery/domain/usecase/AddItemUseCase.kt
@@ -0,0 +1,20 @@
+package com.github.dkw87.ezgrocery.domain.usecase
+
+import com.github.dkw87.ezgrocery.data.repository.GroceryItemRepository
+import com.github.dkw87.ezgrocery.domain.model.GroceryItem
+import java.util.UUID
+
+class AddItemUseCase(private val repository: GroceryItemRepository) {
+    fun execute(name: String, quantity: Int, position: Int) {
+        if (name.isNotBlank()) {
+            val item = GroceryItem(
+                id = UUID.randomUUID().toString(),
+                name = name.trim(),
+                quantity = quantity,
+                position = position,
+                isCompleted = false
+            )
+            repository.addItem(item)
+        }
+    }
+}
