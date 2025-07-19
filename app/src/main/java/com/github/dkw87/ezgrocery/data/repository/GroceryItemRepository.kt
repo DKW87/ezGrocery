d466b969109cecb06e7bfc5ebbd24540bfd407a0
DKW87 <dannykwant@gmail.com>
2025-07-19 21:09:24 +0200

    feat(GroceryItemRepository): adds init

---
+++ app/src/main/java/com/github/dkw87/ezgrocery/data/repository/GroceryItemRepository.kt
@@ -0,0 +1,28 @@
+package com.github.dkw87.ezgrocery.data.repository
+
+import com.github.dkw87.ezgrocery.data.localstorage.GroceryItemDataStorage
+import com.github.dkw87.ezgrocery.domain.model.GroceryItem
+import kotlinx.coroutines.flow.StateFlow
+
+class GroceryItemRepository(private val dataStorage : GroceryItemDataStorage) {
+
+    fun getAllGroceryItems(): StateFlow<List<GroceryItem>> = dataStorage.items
+
+    fun addItem(item: GroceryItem) {
+        dataStorage.addItem(item)
+    }
+
+    fun updateItem(item: GroceryItem) {
+        dataStorage.updateItem(item)
+    }
+
+    fun removeItem(itemID: String) {
+        dataStorage.removeItem(itemID)
+    }
+
+    fun toggleItem(item: GroceryItem) {
+        val toggledItem = item.copy(isCompleted = !item.isCompleted)
+        dataStorage.updateItem(toggledItem)
+    }
+
+}
