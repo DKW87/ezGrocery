393b484745d5ec1bc2e906eaedd39f218271b26b
DKW87 <dannykwant@gmail.com>
2025-07-20 11:49:56 +0200

    refactor(addItemUseCase): set position dynamically

--- app/src/main/java/com/github/dkw87/ezgrocery/domain/usecase/AddItemUseCase.kt
+++ app/src/main/java/com/github/dkw87/ezgrocery/domain/usecase/AddItemUseCase.kt
@@ -5,9 +5,12 @@ import com.github.dkw87.ezgrocery.domain.model.GroceryItem
 import java.util.UUID
 
 class AddItemUseCase(private val repository: GroceryItemRepository) {
-    fun execute(name: String, quantity: Int, position: Int) {
+    fun execute(name: String, quantity: Int) {
         if (name != null) {
             if (name.isNotBlank()) {
+                val allItems = repository.getAllGroceryItems().value
+                val position = if (allItems.isEmpty()) 0 else allItems.maxOf { it.position } + 1
+
                 val item = GroceryItem(
                     id = UUID.randomUUID().toString(),
                     name = name.trim(),
@@ -15,6 +18,7 @@ class AddItemUseCase(private val repository: GroceryItemRepository) {
                     position = position,
                     isCompleted = false
                 )
+
                 repository.addItem(item)
             }
         }
