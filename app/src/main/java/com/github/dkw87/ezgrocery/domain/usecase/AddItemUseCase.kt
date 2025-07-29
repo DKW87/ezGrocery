ebaccaccec35f308f64a2996c8485a80ad395223
DKW87 <dannykwant@gmail.com>
2025-07-29 21:45:30 +0200

    refactor(AddItemUseCase): removes unnecessary null check

--- app/src/main/java/com/github/dkw87/ezgrocery/domain/usecase/AddItemUseCase.kt
+++ app/src/main/java/com/github/dkw87/ezgrocery/domain/usecase/AddItemUseCase.kt
@@ -6,21 +6,19 @@ import java.util.UUID
 
 class AddItemUseCase(private val repository: GroceryItemRepository) {
     fun execute(name: String, quantity: Int) {
-        if (name != null) {
-            if (name.isNotBlank()) {
-                val allItems = repository.getAllGroceryItems().value
-                val position = if (allItems.isEmpty()) 0 else allItems.maxOf { it.position } + 1
+        if (name.isNotBlank()) {
+            val allItems = repository.getAllGroceryItems().value
+            val position = if (allItems.isEmpty()) 0 else allItems.maxOf { it.position } + 1
 
-                val item = GroceryItem(
-                    id = UUID.randomUUID().toString(),
-                    name = name.trim(),
-                    quantity = quantity,
-                    position = position,
-                    isCompleted = false
-                )
+            val item = GroceryItem(
+                id = UUID.randomUUID().toString(),
+                name = name.trim(),
+                quantity = quantity,
+                position = position,
+                isCompleted = false
+            )
 
-                repository.addItem(item)
-            }
+            repository.addItem(item)
         }
     }
 }
