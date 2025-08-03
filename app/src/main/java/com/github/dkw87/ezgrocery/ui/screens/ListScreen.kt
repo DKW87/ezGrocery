59509141c6373c32fbd6c831f4b83db4bcc9a8c3
DKW87 <dannykwant@gmail.com>
2025-08-03 14:34:07 +0200

    refactor(ListScreen): bottombar now takes gesture/button navigation into account

--- app/src/main/java/com/github/dkw87/ezgrocery/ui/screens/ListScreen.kt
+++ app/src/main/java/com/github/dkw87/ezgrocery/ui/screens/ListScreen.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -46,6 +50,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.github.dkw87.ezgrocery.R
 import com.github.dkw87.ezgrocery.domain.model.GroceryItem
@@ -118,7 +123,9 @@ fun ListScreen(
             if (isEditListScreen) {
                 BottomAppBar(
                     containerColor = MaterialTheme.colorScheme.surface,
-                    modifier = Modifier.height(90.dp)
+                    modifier = Modifier.height(
+                        40.dp + WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+                    )
                 ) {
                     Spacer(modifier = Modifier.weight(1f))
 
